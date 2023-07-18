b614c78799a9349ddb06c06abd029b701f87a1f4
3791xk <xeniak3791@gmail.com>
2023-07-17 23:39:25 -0400

    Info Column
    created column for user profile information when browsing

--- rizz/lib/browsing.dart
+++ rizz/lib/browsing.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'consts.dart';
 
-class FakeData {}
+class FakeData {
+  /*  Photos
+      Name,
+      Age,
+      Grade Level??!
+      Pronouns
+      About Me - fill in box
+      Lifestyle + interests 
+  */
+}
 
 class PhotoSwipe extends StatefulWidget {
   const PhotoSwipe({super.key});
@@ -14,10 +24,10 @@ class _PhotoSwipeState extends State<PhotoSwipe> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onHorizontalDragEnd: (DragEndDetails details) {
-        if (details.primaryVelocity < 0) {
+        //if (details.primaryVelocity < 0) {
           // User swiped left
-          print('User swiped left');
-        }
+          //print('User swiped left');
+       // }
       },
       child: const Center(
         child: Text('PhotoSwipe'),
@@ -44,9 +54,73 @@ class _BrowsingPageState extends State<BrowsingPage> {
         child: Column(
           children: [
             Text('Browsing'),
+            InfoCol()
           ],
         ),
       ),
     );
+  }
+}
+
+class InfoCol extends StatelessWidget {
+  const InfoCol({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+      return Center(
+            child: Expanded(
+                child: Container(
+                  padding: Consts.browsePadding,
+                    child: Column(
+                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [  
+                      const Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: Consts.lowPadding,
+                                child: Icon(Icons.face),
+                              ),
+                              Text('Pronouns'),
+                            ],
+                          ),
+                        Divider(
+                          thickness: 1,
+                          color: Theme.of(context).colorScheme.onPrimaryContainer,
+                        ),
+                        // About Me
+                        Container(
+                          padding: Consts.lowPadding,
+                          child: Text(
+                            'About Me',
+                            style: Theme.of(context).textTheme.displayMedium,
+                          ),
+                        ),
+                        Container(
+                          padding: Consts.sidePadding,
+                          child: const Text('about me text here'),
+                        ),
+                        Divider(
+                          thickness: 1,
+                          color: Theme.of(context).colorScheme.onPrimaryContainer,
+                        ),
+                        // Lifestyle and Interests
+                        Container(
+                          padding: Consts.lowPadding,
+                          child: Text(
+                            'Lifestyle and Interests',
+                            style: Theme.of(context).textTheme.displayMedium,
+                          ),
+                        ),
+                        Container(
+                          padding: Consts.sidePadding,
+                          child: const Text('interests go here'),
+                        ),
+                       ],
+                    ),
+                ),
+            ),
+      );
   }
 }
