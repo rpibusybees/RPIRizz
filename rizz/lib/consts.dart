340652eb41963f6e03534789f9b8a798a6a4ab9b
hicker2 <91489334+hicker2@users.noreply.github.com>
2023-07-30 14:07:53 -0400

    added more options to lifestyle stuff + styling
    added more options to the lifestyle pages along with adding styling to the pages

--- rizz/lib/consts.dart
+++ rizz/lib/consts.dart
@@ -8,21 +8,22 @@ class Consts {
       EdgeInsets.symmetric(horizontal: 30, vertical: 100);
 
   // padding used to separate question from the textfield
-  static const EdgeInsets questionPadding = 
-      EdgeInsets.only(bottom: 25);
-  
+  static const EdgeInsets questionPadding = EdgeInsets.only(bottom: 25);
+
   // padding used to give some space from the horizontal bounds
-  static const EdgeInsets fieldPadding =
-    EdgeInsets.only(left: 30, right: 30);
+  static const EdgeInsets fieldPadding = EdgeInsets.only(left: 30, right: 30);
 
   // padding used to space the bottom button out from
   // the other components
   static const EdgeInsets bottomButtonPadding =
-    EdgeInsets.only(top: 50, bottom: 100);
+      EdgeInsets.only(top: 50, bottom: 50);
 
-  // padding used to space the questions in the 
+  // padding used to space the questions in the
   // gender and lifestyle pages
-  static const EdgeInsets titleQuestionPadding =
-    EdgeInsets.only(top: 50);
+  static const EdgeInsets titleQuestionPadding = EdgeInsets.only(top: 50);
 
+  // padding used for containing the lifestyle and other bubbles
+  // in the lifestyle quiz
+  static const EdgeInsets lifestyleBubblePadding =
+      EdgeInsets.only(left: 15, right: 15);
 }
--- rizz/lib/lifestyle.dart
+++ rizz/lib/lifestyle.dart
@@ -2,6 +2,7 @@
 /// the user has. We create a lifestyle button which
 /// has a lifestyle info object in it
 library lifestyle.dart;
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rizz/seeking.dart';
@@ -10,17 +11,16 @@ import 'genderlifestyleinfo.dart';
 import 'nextbutton.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-/// Used to create a 
+/// Used to create a
 /// [LifestylePageState]
-class LifestylePage extends StatefulWidget{
+class LifestylePage extends StatefulWidget {
   const LifestylePage({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => LifestylePageState();
 }
 
-class LifestylePageState extends State<LifestylePage>{
-
+class LifestylePageState extends State<LifestylePage> {
   @override
   void initState() {
     super.initState();
@@ -29,21 +29,21 @@ class LifestylePageState extends State<LifestylePage>{
   List<LifestyleInfo> lifestyles = Lifestyles.getLifestyles();
 
   // returns the likes as a string
-  List<String> getLikes(){
+  List<String> getLikes() {
     List<String> likes = [];
-    for(int i = 0; i < lifestyles.length; i++){
-      if(lifestyles[i].preference == 1){
-         likes.add(lifestyles[i].lifestyle);
+    for (int i = 0; i < lifestyles.length; i++) {
+      if (lifestyles[i].preference == 1) {
+        likes.add(lifestyles[i].lifestyle);
       }
     }
     return likes;
   }
 
   // returns the dislikes as a string
-  List<String> getDislikes(){
+  List<String> getDislikes() {
     List<String> dislikes = [];
-    for(int i = 0; i < lifestyles.length; i++){
-      if(lifestyles[i].preference == 2){
+    for (int i = 0; i < lifestyles.length; i++) {
+      if (lifestyles[i].preference == 2) {
         dislikes.add(lifestyles[i].lifestyle);
       }
     }
@@ -54,51 +54,60 @@ class LifestylePageState extends State<LifestylePage>{
   void uploadLifestyleToDatabase() async {
     User? user = FirebaseAuth.instance.currentUser;
     final db = FirebaseFirestore.instance;
-    await db.collection('users').doc(user!.uid).update({"myLikes": getLikes(), "myDislikes": getDislikes()});
+    await db
+        .collection('users')
+        .doc(user!.uid)
+        .update({"myLikes": getLikes(), "myDislikes": getDislikes()});
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: Consts.lifestyleBubblePadding,
         color: Theme.of(context).colorScheme.background,
         child: Column(
           children: [
             Container(
               padding: Consts.titleQuestionPadding,
               child: Text(
-                'Lifestyles and Interests',
+                'I am...',
                 style: Theme.of(context).textTheme.displayLarge,
                 textAlign: TextAlign.center,
               ),
             ),
+            const SizedBox(height: 10),
+            Text(
+              'Click once to like and twice to dislike',
+              style: Theme.of(context).textTheme.labelMedium,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 25),
             Expanded(
               flex: 1,
-              child: ListView.builder(
-                itemCount: lifestyles.length,
-                itemBuilder: (BuildContext context, int index){
-                  final lifestyle = lifestyles[index];
-                  return LifestyleButton(lifeObj: lifestyle);
-                },
+              child: SingleChildScrollView(
+                child: Wrap(
+                  spacing: 10,
+                  runSpacing: 10,
+                  children: lifestyles
+                      .map((lifestyle) => LifestyleButton(lifeObj: lifestyle))
+                      .toList(),
+                ),
               ),
             ),
-            
             Container(
               margin: Consts.bottomButtonPadding,
-              child: NextButton(
-                onPressed: (){
-                  uploadLifestyleToDatabase();
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const SeekingPage()),
-                  );
-
-                }
-              ),
+              child: NextButton(onPressed: () {
+                uploadLifestyleToDatabase();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SeekingPage()),
+                );
+              }),
             ),
           ],
         ),
       ),
-    );    
+    );
   }
 }
